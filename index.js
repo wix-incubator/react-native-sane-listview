f552b5840e3f5f941bf4c48879a1f6bd607dfc33
Daniel Zlotin <danielzl@wix.com>
2016-09-23 14:27:46 +0300

    src and lint

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
+import {SaneListView} from 'src/SaneListView';
 
-import React, {Component} from 'react';
-import {ListView} from 'react-native';
-
-const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-
-export default class SaneListView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: ds.cloneWithRows(this.props.rowsById, this.props.rowsIdArray)
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.rowsById !== nextProps.rowsById) {
-      this.setState({
-        dataSource: ds.cloneWithRows(nextProps.rowsById, nextProps.rowsIdArray)
-      });
-    }
-  }
-
-  render() {
-    return (
-      <ListView
-        {...this.props}
-        dataSource={this.state.dataSource}
-        />
-    );
-  }
-}
+module.exports = {
+  SaneListView
+};
